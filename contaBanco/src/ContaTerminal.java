a76c44bbba95362da583e49cbd8dafd1ca21d0ca
Gabriel <prado1939@live.com>
2025-01-07 20:27:44 -0300

    termino 2.0

--- contaBanco/src/ContaTerminal.java
+++ contaBanco/src/ContaTerminal.java
@@ -23,33 +23,4 @@ public class ContaTerminal {
     }
 }
 
-// import java.util.Scanner;
 
-// public class ContaTerminal {
-//     public static void main(String[] args) {
-//         try (Scanner scanner = new Scanner(System.in)) {
-//             // Solicitar e capturar número da conta
-//             System.out.println("Digite o número da sua conta bancária:");
-//             int conta = scanner.nextInt();
-
-//             // Solicitar e capturar número da agência
-//             System.out.println("Agora, digite o número da sua agência bancária:");
-//             scanner.nextLine(); // Consumir o "\n" pendente
-//             String agencia = scanner.nextLine();
-
-//             // Solicitar e capturar o nome completo
-//             System.out.println("Digite o seu nome completo:");
-//             String nome = scanner.nextLine();
-
-//             // Solicitar e capturar o saldo
-//             System.out.println("Digite agora o saldo que devera ser retirado da conta bancaria:");
-//             double saldo = scanner.nextDouble();
-
-//             // Exibir mensagem formatada
-//             System.out.println(String.format(
-//                 "Olá %s, obrigado por criar uma conta em nosso banco! Sua agência é %s, conta %d, e seu saldo de R$ %.2f já está disponível para saque.",
-//                 nome, agencia, conta, saldo
-//             ));
-//         }
-//     }
-// }
